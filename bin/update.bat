029d6823283b1cf3f829969c70cb53f42a2547d7
dodo <perfectdodo@gmail.com>
2026-04-03 13:15:16 +0800

    bin\update: Commit only manifests upgraded by it

--- bin/update.bat
+++ bin/update.bat
@@ -10,7 +10,13 @@ if "%PWSH%x" == "x" for /f %%i in ('scoop which powershell') do set PWSH=%%i
 
 for %%i in (%0) do set DIR=%%~dpi..
 cd %DIR%
-git checkout master
+git checkout uo
+
+set "TMP_PRE=%TEMP%\scoop_pre_%RANDOM%.txt"
+set "TMP_POST=%TEMP%\scoop_post_%RANDOM%.txt"
+set "TMP_UPD=%TEMP%\scoop_upd_%RANDOM%.txt"
+set "TMP_POST_MAN=%TEMP%\scoop_post_man_%RANDOM%.txt"
+git diff --name-only > "%TMP_PRE%"
 
 if "x%~1" == "x" goto updall
 
@@ -26,13 +32,17 @@ goto :updone
 if errorlevel 1 exit /b %errorlevel%
 
 :commit
-for /f %%i in ('git diff --name-only') do (
+git diff --name-only > "%TMP_POST%"
+findstr /i /r /c:"^bucket\\.*\.json$" "%TMP_POST%" > "%TMP_POST_MAN%"
+findstr /v /x /g:"%TMP_PRE%" "%TMP_POST_MAN%" > "%TMP_UPD%"
+for /f %%i in ('type "%TMP_UPD%"') do (
     git add %%i
     for /f %%v in ('jq .version %%i') do (
         git commit -m "%%~ni: Update to version %%v"
         git tag "%%~ni-%%v"
     )
 )
+del "%TMP_PRE%" "%TMP_POST%" "%TMP_POST_MAN%" "%TMP_UPD%" >nul 2>nul
 ::git push origin master
 ::git checkout -f master
 
